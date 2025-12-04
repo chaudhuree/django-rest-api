d56d387c88e197a5b75ea96abc661c13176c09e8
chaudhuree <chaudhuree00@gmail.com>
2025-12-04 13:34:21 +0600

    add POST method to students view for creating new student records

--- api/views.py
+++ api/views.py
@@ -4,9 +4,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def students(request):
     if request.method == 'GET':
         student_list = Student.objects.all()
         serializer = StudentSerializer(student_list, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
