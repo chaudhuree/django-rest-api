57b25fd8f8430cce1cd96f8e5d9ee80d84f57b23
chaudhuree <chaudhuree00@gmail.com>
2025-12-04 16:21:49 +0600

    keep only code that is related to mixins

--- api/serializers.py
+++ api/serializers.py
@@ -1,11 +1,5 @@
 from rest_framework import serializers
-from students.models import Student
 from employees.models import Employee
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = '__all__'
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
--- django_rest_main/urls.py
+++ django_rest_main/urls.py
@@ -19,8 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # web app urls
-    path('students/', include('students.urls')),
 
     # api urls
     path('api/v1/', include('api.urls')),
