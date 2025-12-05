ae68c0c19795cc793c0da69c4d959d5b91332eb8
chaudhuree <chaudhuree00@gmail.com>
2025-12-05 15:04:51 +0600

    add global pagination configuration with PageNumberPagination and PAGE_SIZE of 10 in REST_FRAMEWORK settings

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -120,3 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+
+# Pagination Setup 
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
